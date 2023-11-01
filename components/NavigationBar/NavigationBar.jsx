5507fe9b2d365135bc2de2a36c5712119b83fb31
Hanist0209 <jujohn96@gmail.com>
2023-11-01 14:25:36 +0900

    테일윈드,컴포넌트

---
+++ components/NavigationBar/NavigationBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NavigationBar () {
+    return ( 
+    <div className='bg-slate-900 py-4 px-[3rem] flex justify-between text-white'>
+        <div className='bg-red-700 inline-block'>LOGO</div>
+        <div className='bg-red-700 inline-block'>LOGO</div>
+    </div> )
+}
